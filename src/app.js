f91a7af4695d93898fd75481915d84a094449ef1
omar-albakri <albakrio@outlook.com>
2020-03-17 12:23:51 -0700

    removed mocked data

--- src/app.js
+++ src/app.js
@@ -13,15 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 44 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 0, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 100 }));
-//store.dispatch(setTextFilter('water'));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
 	<div>
 		<Provider store={store}>
